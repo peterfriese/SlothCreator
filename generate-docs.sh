3efd78592745cdad74698954993f4736e1ae1555
Peter Friese <peter@peterfriese.de>
2022-09-18 12:38:58 +0200

    💚 Update deployment path

--- generate-docs.sh
+++ generate-docs.sh
@@ -2,4 +2,5 @@ swift package --allow-writing-to-directory docs \
     generate-documentation \
     --disable-indexing \
     --transform-for-static-hosting \
+    --hosting-base-path SlothCreator \
     --output-path docs
